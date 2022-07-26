db695edb5a46ff8f704b9d703f17e2a868ef61fe
Joel Brinson <jtbrinson11@gmail.com>
2022-07-26 16:00:47 -0400

    Initialized files

---
+++ src/main/java/com/GenSpark/RESTFulApp_2/RestFulApp2Application.java
@@ -0,0 +1,13 @@
+package com.GenSpark.RESTFulApp_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/GenSpark/RESTFulApp_2/RestFulApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GenSpark.RESTFulApp_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
