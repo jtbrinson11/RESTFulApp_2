ac1beb46235d7fd623a83907568ee8304cacdcf8
Joel Brinson <jtbrinson11@gmail.com>
2022-07-27 13:47:34 -0400

    Finished project

--- src/main/java/com/GenSpark/RESTFulApp_2/Service/AirplaneServiceImpl.java
+++ src/main/java/com/GenSpark/RESTFulApp_2/Service/AirplaneServiceImpl.java
@@ -5,7 +5,6 @@ import com.GenSpark.RESTFulApp_2.Entity.Airplane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
